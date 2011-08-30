d43f35abd4ee8be2eeed1ee6122e551760c2a95e
David Wilkins <david@wilkox.org>
2011-08-30 16:22:57 +1000

    Generated 'texts' model

---
+++ app/models/texts.rb
@@ -0,0 +1,3 @@
+class Texts < ActiveRecord::Base
+  attr_accessible :content
+end
---
+++ db/migrate/20110830062123_create_texts.rb
@@ -0,0 +1,13 @@
+class CreateTexts < ActiveRecord::Migration
+  def self.up
+    create_table :texts do |t|
+      t.string :content
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :texts
+  end
+end
