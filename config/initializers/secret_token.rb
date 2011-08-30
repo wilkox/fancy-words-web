991ea60c0cfdd4994d52921ca8f11cb6c96ddb6b
David Wilkins <david@wilkox.org>
2011-08-30 16:12:08 +1000

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FancyWordsWeb::Application.config.secret_token = 'abe505cd426980f2ac4f99cf2ec0cc23402440b2e3336fd72f741d6fb9ea246c66caa9a1f84b693253dfd1af44ca4a5981e01ecb2e933e765dd5e0b4bb10fbbf'
